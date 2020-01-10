17e5c7b60dd0092713a455c2600e2cf10fbcf189
Jessica Bassard <53714967+jbassard88@users.noreply.github.com>
2020-01-09 17:15:45 -0800

    functionality

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const button = (props) => {
     const classes = ['btn'];
-    if (typeof props !== 'undefined' && typeof props.type !== 'undefined')
-        classes.push('btn--' + props.type);
-    return (
-        <button className={classes.join(' ')}>
+if (typeof props !== 'undefined' && typeof props.type !== 'undefined')
+    classes.push('btn--' + props.type);
+return (
+        <button className={classes.join(' ')} onClick={props.onButtonPress}>
             {props.children}
         </button>
     );
